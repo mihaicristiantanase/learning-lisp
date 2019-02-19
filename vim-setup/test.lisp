a25d9eec7eb7228bfa4eb8d549d90dbb47432512
Mihai Cristian Tanase <mihaicristian.tanase@gmail.com>
2019-02-19 21:33:11 +0200

    update vim-setup

--- vim-setup/test.lisp
+++ vim-setup/test.lisp
@@ -22,7 +22,11 @@
 (add-user "flory" 10)
 (add-user "costi" 10)
 
-(format t "~{~a~%~}" *db*)
+(format t "~{~a:~10t~%~}" *db*)
+
+(list :mihai 10)
+
+(format t "~a:~10t~a~%" :mihai 10)
 
 (describe '(1 2 3))
 
@@ -78,3 +82,31 @@
 (documentation 'fibbo-improved2 'function)
 
 (describe 'fibbo-improved2)
+
+(format t "mihai is here!~%")
+
+(defun plot (f &optional (minx 0) (maxx 10) (step 1))
+  (loop for x from minx below maxx by step do
+        (loop repeat (funcall f x) do (format t "#"))
+        (format t "~%")))
+(plot #'(lambda (x) (* x 7)))
+
+(defparameter x 0.5)
+x
+(setf x 10)
+
+(defparameter *lst* '())
+(loop repeat 100 do (push (random 100) *lst*))
+(dolist (x *lst*)
+  (format t "~d " x))
+
+(defparameter *array* (make-array '(40)))
+(loop repeat 10000 do
+      (incf (aref *array* (random (length *array*)))))
+(loop for i from 0 below (length *array*) by 1 do
+      (format t "~d~%" (aref *array* i)))
+
+(defparameter x 1)
+(defparameter y 2)
+(rotatef x y)
+(format t "~d ~d" x y)
