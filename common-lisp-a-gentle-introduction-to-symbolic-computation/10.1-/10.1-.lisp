bf7de720019adf2b317d45a48831a0261abc7fda
Mihai Cristian Tanase <mihaicristian.tanase@gmail.com>
2019-06-16 08:40:59 +0300

    Update 10.1-.lisp

--- common-lisp-a-gentle-introduction-to-symbolic-computation/10.1-/10.1-.lisp
+++ common-lisp-a-gentle-introduction-to-symbolic-computation/10.1-/10.1-.lisp
@@ -41,3 +41,24 @@
 
 (forget 'cucu)
 (forget 'monika)
+
+;; 10.5
+(defun not-so-ugly (x y)
+  (let* ((m (max x y))
+         (avg (/ (+ x y) 2.0))
+         (pct (* 100 (/ avg m))))
+    (list 'average avg 'is pct 'percent 'of 'max m)))
+
+(not-so-ugly 10 20)
+
+;; 10.6
+(defparameter x nil)
+(push x x)
+(push x x)
+(push x x)
+
+;; 10.7
+(defparameter x '(1 2 3))
+(setf (length x) 99) ; does not work because "length" does not specify a valid place description
+(setf (nth 2 x) 99) ; works
+x
