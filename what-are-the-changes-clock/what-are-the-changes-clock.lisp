d51e895f251c61c3cd75d3e649a842dff0c260a9
Mihai Cristian Tanase <mihaicristian.tanase@gmail.com>
2019-09-30 21:56:00 +0300

    Add what-are-the-changes-clock.lisp

---
+++ what-are-the-changes-clock/what-are-the-changes-clock.lisp
@@ -0,0 +1,36 @@
+;; Computes the chances that you see a digital clock combination
+
+(defun add-entrire-hour (h)
+  (let ((lst '()))
+    (dotimes (m 60)
+      (push (format nil "~2,'0d:~2,'0d" h m) lst))
+    (reverse lst)))
+
+(defun gen-time-table (tf)
+  (let* ((str (write-to-string tf))
+         (nr-hours (parse-integer (subseq str 3)))
+         (lst '()))
+    (dotimes (h nr-hours)
+      (setf lst (append lst (add-entrire-hour h))))
+    (when (equal 'fmt12 tf)
+      (do ((h 12 (1+ h)))
+          ((> h 24))
+          (when (not (= h 13))
+            (setf lst (append lst (add-entrire-hour (mod h 13)))))))
+    lst))
+
+(defun cell-gen-table (format)
+  (cons format (gen-time-table format)))
+
+(defparameter time-formats
+  (list
+    (cell-gen-table 'fmt12)
+    (cell-gen-table 'fmt24)))
+
+(defun what-are-the-chances (time format)
+  (let* ((pos (position #\: time))
+         (hour (subseq time 0 pos))
+         (minute (subseq time (+ pos 1))))
+    (count time (cdr (assoc format time-formats)) :test 'string=)))
+
+(what-are-the-chances "00:12" 'fmt12)
