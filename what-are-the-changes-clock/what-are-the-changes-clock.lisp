4b76f129d90b9b7abd0bd85526cf667f8935ebae
Mihai Cristian Tanase <mihaicristian.tanase@gmail.com>
2019-10-01 21:18:38 +0300

    Add predicates logic for what-are-the-changes-clock.lisp

--- what-are-the-changes-clock/what-are-the-changes-clock.lisp
+++ what-are-the-changes-clock/what-are-the-changes-clock.lisp
@@ -1,4 +1,4 @@
-;; Computes the chances that you see a digital clock combination
+;;; Computes the chances that you see a digital clock combination
 
 (defun add-entrire-hour (h)
   (let ((lst '()))
@@ -27,10 +27,35 @@
     (cell-gen-table 'fmt12)
     (cell-gen-table 'fmt24)))
 
-(defun what-are-the-chances (time format)
-  (let* ((pos (position #\: time))
-         (hour (subseq time 0 pos))
-         (minute (subseq time (+ pos 1))))
-    (count time (cdr (assoc format time-formats)) :test 'string=)))
+;; predicates
 
-(what-are-the-chances "00:12" 'fmt12)
+(defun perfect-match (val candidate tfs)
+  (string= val candidate))
+
+(defun later-than (val candidate tfs)
+  (< (position val tfs :test #'string=)
+     (position candidate tfs :test #'string=)))
+
+(defun four-equal (candidate tfs)
+  (let ((first-char (aref candidate 0)))
+    (every (lambda (x)
+             (equal x first-char))
+           (remove #\: candidate))))
+
+;; main processor
+
+(defun what-are-the-chances (predicate format)
+  (let* ((fun (first predicate))
+         (fun-args (rest predicate))
+         (tfs (cdr (assoc format time-formats)))
+         (items (remove-if-not
+                  (lambda (x) (apply fun (append fun-args (list x tfs))))
+                  tfs)))
+    (format t "items = ~a" items)
+    (length items)))
+
+;; tests
+
+(what-are-the-chances '(perfect-match "01:59") 'fmt12)
+(what-are-the-chances '(later-than "23:58") 'fmt24)
+(what-are-the-chances '(four-equal) 'fmt24)
