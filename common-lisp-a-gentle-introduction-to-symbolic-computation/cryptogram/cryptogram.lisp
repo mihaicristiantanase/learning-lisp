eb13ce2fe185890b8ab05ad6e222e81047851dd7
Mihai Cristian Tanase <mihaicristian.tanase@gmail.com>
2019-12-08 15:43:54 +0200

    Add initial work on cryptogram.lisp

--- common-lisp-a-gentle-introduction-to-symbolic-computation/cryptogram/cryptogram.lisp
+++ common-lisp-a-gentle-introduction-to-symbolic-computation/cryptogram/cryptogram.lisp
@@ -1,3 +1,29 @@
 ;; Decipher the cryptogram
 
-(print "An we begin")
+(print "And we begin")
+
+(defvar *crypto-text*
+  '("zj ze kljjls jf slapzi ezvlij pib kl jufwxuj p hffv"
+    "jupi jf enlpo pib slafml pvv bfwkj"))
+
+(defun display-status (encrypted decipher-table)
+  (format t "~&----------------------------------------")
+  (dolist (s encrypted)
+    (format t "~&~a" s))
+  (format t "~&----------------------------------------"))
+
+;; TODO(mihai): fix this
+(subst #\a #\b "abcdefc")
+
+(defun solve-locally (encrypted encipher-table decipher-table)
+  (let ((decipher-table '())
+        (encipher-table '()))
+    (display-status encrypted decipher-table)
+    (format t "~&Substitute which letter?")))
+
+(defun solve (encrypted)
+  (let ((encipher-table '())
+        (decipher-table '()))
+    (solve-locally encrypted encipher-table decipher-table)))
+
+(solve *crypto-text*)
