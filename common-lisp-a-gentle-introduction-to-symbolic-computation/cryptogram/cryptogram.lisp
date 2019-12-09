c687b19f9ffca19ff546316b637b1bf9fef2108f
Mihai Cristian Tanase <mihaicristian.tanase@gmail.com>
2019-12-09 20:13:38 +0200

    Add more work to cryptogram.lisp

--- common-lisp-a-gentle-introduction-to-symbolic-computation/cryptogram/cryptogram.lisp
+++ common-lisp-a-gentle-introduction-to-symbolic-computation/cryptogram/cryptogram.lisp
@@ -6,14 +6,21 @@
   '("zj ze kljjls jf slapzi ezvlij pib kl jufwxuj p hffv"
     "jupi jf enlpo pib slafml pvv bfwkj"))
 
+(defun decipher (encrypted decipher-table)
+  (if (null decipher-table)
+      encrypted
+      (let ((tuple (first decipher-table)))
+            (decipher (substitute (car tuple) (cdr tuple) encrypted)
+                      (rest decipher-table)))))
+
 (defun display-status (encrypted decipher-table)
   (format t "~&----------------------------------------")
   (dolist (s encrypted)
-    (format t "~&~a" s))
+    (format t "~&~a" s)
+    (format t "~&~a" (decipher s decipher-table)))
   (format t "~&----------------------------------------"))
 
-;; TODO(mihai): fix this
-(subst #\a #\b "abcdefc")
+(substitute  #\a #\b "ababbfb")
 
 (defun solve-locally (encrypted encipher-table decipher-table)
   (let ((decipher-table '())
